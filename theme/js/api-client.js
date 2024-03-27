977ea57b91cf8f659b651d5abbf4864022a8ea63
jrichardsz <jrichardsz.java@gmail.com>
2024-03-26 19:49:54 -0500

    fix issue #18 _entrypoint.js

--- theme/js/api-client.js
+++ theme/js/api-client.js
@@ -1,4 +1,4 @@
-function ApiClient(){
+function ApiClient() {
 
   var apiBaseUrl = getLocationBasePath();
   var database = new loki('database.db');
@@ -10,11 +10,11 @@ function ApiClient(){
 
   this.loadDatabase = () => {
     console.log("Loading database");
-    return new Promise(function(resolve, reject) {
-      $.getJSON("./database.json", function(data) {
+    return new Promise(function (resolve, reject) {
+      $.getJSON("./database.json", function (data) {
         database.loadJSONObject(data);
         resolve()
-      }).fail(function() {
+      }).fail(function () {
         console.log("An error has occurred.");
         reject();
       });
@@ -26,16 +26,16 @@ function ApiClient(){
   }
 
   this.findAll = () => {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       resolve(database.getCollection('documents').data);
     });
   };
 
   this.findAllPaginated = (pageSize, pageNumber) => {
-    return new Promise(function(resolve, reject) {
-      var allItems = database.getCollection('documents').data;      
-      
-      allItems.sort(function(a,b){
+    return new Promise(function (resolve, reject) {
+      var allItems = database.getCollection('documents').data;
+
+      allItems.sort(function (a, b) {
         var rawDate1 = a.datetime.split("-");
         var first = new Date(rawDate1[0], rawDate1[1], rawDate1[2]);
         var rawDate2 = b.datetime.split("-");
@@ -47,23 +47,23 @@ function ApiClient(){
       var result = {
         "content": paginate(allItems, pageSize, pageNumber),
         "pagination": {
-            "totalElements": allItems.length,
-            "pagesCount": allItems.length/pageSize,
-            "pageNumber": pageNumber,
-            "pageSize": pageSize
+          "totalElements": allItems.length,
+          "pagesCount": allItems.length / pageSize,
+          "pageNumber": pageNumber,
+          "pageSize": pageSize
         }
       }
       resolve(result);
     });
-  };  
+  };
 
   function paginate(array, page_size, page_number) {
     // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
     return array.slice((page_number - 1) * page_size, page_number * page_size);
-  }  
+  }
 
   this.findDocumentByPath = (path) => {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var query = [
         {
           "path": path
@@ -79,11 +79,11 @@ function ApiClient(){
   };
 
   this.findDocumentByText = (text) => {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var queryCollection = [
         {
           "text": {
-            "$regex": [text,"i"]
+            "$regex": [text, "i"]
           }
         }
       ];
@@ -103,12 +103,12 @@ function ApiClient(){
   * @param {string} value
   */
   function addParam(url, param, value) {
-     param = encodeURIComponent(param);
-     var a = document.createElement('a');
-     param += (value ? "=" + encodeURIComponent(value) : "");
-     a.href = url;
-     a.search += (a.search ? "&" : "") + param;
-     return a.href;
+    param = encodeURIComponent(param);
+    var a = document.createElement('a');
+    param += (value ? "=" + encodeURIComponent(value) : "");
+    a.href = url;
+    a.search += (a.search ? "&" : "") + param;
+    return a.href;
   }
 
   function getLocationBasePath() {
@@ -123,16 +123,16 @@ function ApiClient(){
       return;
     }
 
-    if(window.location.port){
-      return window.location.protocol+"//"+window.location.hostname+":"+window.location.port
-    }else {
-      return window.location.protocol+"//"+window.location.hostname
+    if (window.location.port) {
+      return window.location.protocol + "//" + window.location.hostname + ":" + window.location.port
+    } else {
+      return window.location.protocol + "//" + window.location.hostname
     }
   }
 
 }
 
-if(typeof window._context === 'undefined'){
-   window._context = {};
+if (typeof window._context === 'undefined') {
+  window._context = {};
 }
-window._context["ApiClient"] =  new ApiClient();
+window._context["ApiClient"] = new ApiClient();
--- theme/js/home-podcast-controller.js
+++ theme/js/home-podcast-controller.js
@@ -1,20 +1,19 @@
-function HomePodcastController(){
+function HomePodcastController() {
 
     var defaultPageSize = 2;
 
     this.apiClient = window._context["ApiClient"];
-    
+
     this.init = async () => {
         this.loadPoscastEntries(1);
     }
-  
-    this.loadPoscastEntries = async(pageNumber) => {
+
+    this.loadPoscastEntries = async (pageNumber) => {
 
         var data = await this.apiClient.findAllPaginated(defaultPageSize, pageNumber);
-        console.log("data", data)
         renderPosts(data.content);
         renderPagination(data.pagination, pageNumber);
-        
+
         //instantiate mp3 player
         var mediaElements = document.querySelectorAll('video, audio'), total = mediaElements.length;
 
@@ -23,25 +22,24 @@ function HomePodcastController(){
                 pluginPath: 'https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/',
                 shimScriptAccess: 'always',
                 success: function () {
-                var target = document.body.querySelectorAll('.player'), targetTotal = target.length;
-                for (var j = 0; j < targetTotal; j++) {
-                    target[j].style.visibility = 'visible';
-                }
+                    var target = document.body.querySelectorAll('.player'), targetTotal = target.length;
+                    for (var j = 0; j < targetTotal; j++) {
+                        target[j].style.visibility = 'visible';
+                    }
                 }
             });
         }
     }
 
-    function renderPosts(podcasts){
-        console.log(document.getElementById('handlebar_template_podcast_list'))
+    function renderPosts(podcasts) {
         var templateSource = document.getElementById('handlebar_template_podcast_list').innerHTML;
         var template = Handlebars.compile(templateSource);
-        
+
         //add post_url
         var newPodcasts = [];
-        for(var podcastInfo of podcasts){
+        for (var podcastInfo of podcasts) {
             var newPodcastInfo = podcastInfo;
-            newPodcastInfo.post_url = "/posts"+podcastInfo.path.replace(".md", ".html")
+            newPodcastInfo.post_url = "/posts" + podcastInfo.path.replace(".md", ".html")
             newPodcasts.push(newPodcastInfo)
         }
 
@@ -49,16 +47,14 @@ function HomePodcastController(){
         var data = {
             podcasts: newPodcasts
         };
-    
+
         // Generate html using template and data
         var html = template(data);
         // Add the result to the DOM
         document.getElementById('podcasts_container').innerHTML = html;
-    }   
-    
-    function renderPagination(paginationInfo, pageNumber){
-        console.log(paginationInfo)
-        
+    }
+
+    function renderPagination(paginationInfo, pageNumber) {
         var paginationContainer = document.getElementById("pagination_footer");
         //remove pre items
         paginationContainer.innerHTML = "";
@@ -69,48 +65,48 @@ function HomePodcastController(){
         var liLeft = document.createElement("li");
         liLeft.style.marginRight = "5px";
         var aLeft = document.createElement('a');
-        aLeft.setAttribute('href',"#");
+        aLeft.setAttribute('href', "#");
         aLeft.classList.add("icon-keyboard_arrow_left")
         liLeft.appendChild(aLeft);
-        ul.appendChild(liLeft);    
+        ul.appendChild(liLeft);
 
         //add items        
-        for(var count=0; count<paginationInfo.pagesCount; count ++){
+        for (var count = 0; count < paginationInfo.pagesCount; count++) {
             var li = document.createElement("li");
             li.style.marginRight = "5px";
             var a = document.createElement('a');
             //a.setAttribute('href',"#");
-            a.setAttribute("page-number", count+1);
+            a.setAttribute("page-number", count + 1);
             a.style.cursor = "pointer";
-            a.innerHTML = count+1; 
-            if(count+1==pageNumber) li.classList.add("active");
+            a.innerHTML = count + 1;
+            if (count + 1 == pageNumber) li.classList.add("active");
             li.appendChild(a);
-            ul.appendChild(li);             
+            ul.appendChild(li);
             li.addEventListener("click", onPageNumberClick)
         }
 
         //add right arrow
         var liRight = document.createElement("li");
         var aRight = document.createElement('a');
-        aRight.setAttribute('href',"#");
+        aRight.setAttribute('href', "#");
         aRight.classList.add("icon-keyboard_arrow_right")
         liRight.appendChild(aRight);
-        ul.appendChild(liRight);            
+        ul.appendChild(liRight);
 
         paginationContainer.appendChild(ul);
-       
+
     }
 
-    onPageNumberClick = (element) =>{
+    onPageNumberClick = (element) => {
         var pageNumber = element.target.getAttribute("page-number");
         this.loadPoscastEntries(new Number(pageNumber));
         var paginationHeader = document.getElementById("pagination_header");
         paginationHeader.focus()
-    }    
+    }
 
-  }
-  
-  if(typeof window._context === 'undefined'){
-     window._context = {};
-  }
-  window._context["HomePodcastController"] =  new HomePodcastController();
+}
+
+if (typeof window._context === 'undefined') {
+    window._context = {};
+}
+window._context["HomePodcastController"] = new HomePodcastController();
